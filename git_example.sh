0a19271571f8a4eed9f81566a1cc6dcfa3305be2
Bill Seremetis <bill@seremetis.net>
2018-10-15 14:17:48 +0300

    Quit the program only after informing the user.

--- git_example.sh
+++ git_example.sh
@@ -7,4 +7,15 @@ then
 
 else
   echo "Hello $1 $2!"
+  read -p "Hammer a key to exit." -n 1 -r
+  echo # Optional move to a new line
+  if [[ $REPLY =~ ^[Aa]$ ]]
+  then
+    echo "A got hammered. Nice!"
+    exit 1
+  else
+    echo "This was not the a key!"
+    echo "I'll quit anyway though."
+  fi
+  echo "Ciao"
 fi
